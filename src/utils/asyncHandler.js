5ca884d7eba2bf2b772981bceb5ba3789199b3c2
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2025-04-09 12:35:17 +0530

    errorhandling

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => {err})
+    }
+}
+
+
+
+export { asyncHandler }
+
+
+
+
+
+
+
+
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             succes: true,
+//             message: error.message
+//         })
+//     }
+// }
