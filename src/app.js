d6d5819d89cbc27a8ba96080360daba86222a616
Abhimanyu kumar <166692284+abhi-webdev@users.noreply.github.com>
2025-04-10 10:32:08 +0530

    routes, controllers setup

--- src/app.js
+++ src/app.js
@@ -17,4 +17,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// Routes ------------------------------------------
+import userRouter from "./routes/user.routes.js"
+
+
+// routes decleration 
+app.use("/api/v1/users", userRouter)   //http://localhost:8000/api/v1/users/register
+
+
+
+
 export { app }
